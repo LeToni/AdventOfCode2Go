69910c18bd80b3c02dd7b86a086468cd78127ff6
Toni Le <toni.le@outlook.de>
2021-01-01 21:28:34 +0100

    Day03 complete

--- AOC_2016/day03/day03.go
+++ AOC_2016/day03/day03.go
@@ -20,14 +20,14 @@ func (triangle *Triangle) IsTriangle() bool {
 	}
 }
 
-func main() {
-	file, err := os.Open("input.txt")
-	defer file.Close()
+func TaskOne() {
+	input_1, err := os.Open("input.txt")
+	defer input_1.Close()
 	if err != nil {
 		panic(err)
 	}
 
-	scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(input_1)
 	count := 0
 	for scanner.Scan() {
 		input := scanner.Text()
@@ -44,5 +44,65 @@ func main() {
 			panic(n)
 		}
 	}
-	fmt.Println("Number of triangles found: ", count)
+	fmt.Println("Task 1: Number of triangles found: ", count)
+}
+
+func TaskTwo() {
+	input_2, err := os.Open("input.txt")
+	defer input_2.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(input_2)
+
+	var (
+		triangle1, triangle2, triangle3 Triangle
+		i                               = 0
+		count                           = 0
+	)
+
+	for scanner.Scan() {
+		input := scanner.Text()
+		var (
+			side1, side2, side3 int
+		)
+		if n, _ := fmt.Sscanf(input, "  %d  %d  %d", &side1, &side2, &side3); n == 3 {
+			switch i {
+			case 0:
+				triangle1.sideA = side1
+				triangle2.sideA = side2
+				triangle3.sideA = side3
+				i++
+			case 1:
+				triangle1.sideB = side1
+				triangle2.sideB = side2
+				triangle3.sideB = side3
+				i++
+			case 2:
+				triangle1.sideC = side1
+				triangle2.sideC = side2
+				triangle3.sideC = side3
+				i = 0
+				if triangle1.IsTriangle() {
+					count++
+				}
+				if triangle2.IsTriangle() {
+					count++
+				}
+				if triangle3.IsTriangle() {
+					count++
+				}
+			}
+		} else {
+			panic(n)
+		}
+
+	}
+	fmt.Println("Task 2: Number of triangles found: ", count)
+}
+
+func main() {
+	TaskOne()
+	TaskTwo()
 }
