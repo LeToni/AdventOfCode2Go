4470d171667988823d64bfdb516af55a4acaa33e
Toni Le <toni.le@outlook.de>
2021-01-03 18:36:59 +0100

    adding helper methods (finding out neighbors and if seat is occupied

--- AOC_2020/day11/day11.go
+++ AOC_2020/day11/day11.go
@@ -1,1 +1,39 @@
 package main
+
+type Position struct {
+	x, y int
+}
+
+func (position *Position) withinSeatingArea() bool {
+	return !(position.x < 0 || position.x >= len(seats) || position.y < len(seats[position.x]))
+}
+
+func (position *Position) isOccupied() bool {
+	if position.withinSeatingArea() && seats[position.x][position.y] == '#' {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (position *Position) occupiedSeatNeighbors() int {
+	count := 0
+	for _, direction := range directions {
+		pos := position.adjacentSeatAt(direction)
+		if pos.isOccupied() {
+			count++
+		}
+	}
+	return count
+}
+
+func (position *Position) adjacentSeatAt(direction Position) *Position {
+	return &Position{position.x + direction.x, position.y + direction.y}
+}
+
+var (
+	directions = []Position{
+		{0, -1}, {1, -1}, {1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1},
+	}
+	seats [][]byte
+)
