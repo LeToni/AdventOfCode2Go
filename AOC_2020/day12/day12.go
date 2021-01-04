93f89aaa71b0ab8a44fddb61bcf1c6294c70f0d7
Toni Le <toni.le@outlook.de>
2021-01-04 20:08:39 +0100

    day12 first half complete

--- AOC_2020/day12/day12.go
+++ AOC_2020/day12/day12.go
@@ -1,20 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"regexp"
+	"strconv"
+)
 
 const (
 	N int = iota
 	E
 	S
 	W
-	R
-	L
-	F
 )
 
 type Instruction struct {
-	action int
+	action rune
 	value  int
+}
+
+func (instruct *Instruction) executeOn(ship *Ship) {
+	if instruct.action == 'R' || instruct.action == 'L' {
+		ship.turn(instruct.action, instruct.value)
+	} else if instruct.action == 'F' {
+		ship.move(instruct.value)
+	} else {
+		ship.navigate(instruct.action, instruct.value)
+	}
 }
 
 type Ship struct {
@@ -51,14 +65,38 @@ func (ship *Ship) move(units int) {
 	}
 }
 
-func (ship *Ship) turn(direction rune) {
+func (ship *Ship) turn(direction rune, turnRadius int) {
 	if direction == 'R' {
-		ship.facing = (ship.facing + 1) % 4
+		ship.facing = (ship.facing + (turnRadius / 90)) % 4
 	} else {
-		ship.facing = ((ship.facing - 1) + 4) % 4
+		ship.facing = ((ship.facing - (turnRadius / 90)) + 4) % 4
 	}
 }
 
 func main() {
+	file, err := os.Open("input.txt")
+	defer file.Close()
+	if err != nil {
+		panic(err)
+	}
 
+	regInstructionFilter := regexp.MustCompile(`(\w)(\d+)`)
+	instructions := []Instruction{}
+	for scanner := bufio.NewScanner(file); scanner.Scan(); {
+		input := scanner.Text()
+		filteredInput := regInstructionFilter.FindStringSubmatch(input)
+		action := []rune(filteredInput[1])[0]
+		value, _ := strconv.Atoi(filteredInput[2])
+		instruction := Instruction{action, value}
+		instructions = append(instructions, instruction)
+	}
+
+	ship := &Ship{x: 0, y: 0, facing: E}
+
+	for _, instruction := range instructions {
+		instruction.executeOn(ship)
+	}
+
+	result := math.Abs(float64(ship.x)) + math.Abs(float64(ship.y))
+	fmt.Println("Task1 -> Distance between starting point and ship:", result)
 }
