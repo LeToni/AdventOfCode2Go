8c8b1522be6c4466d9ca496c7e86464c2f0aec3d
Toni Le <toni.le@outlook.de>
2021-01-04 18:07:57 +0100

    Update day11: split main into different functions

--- AOC_2020/day11/day11.go
+++ AOC_2020/day11/day11.go
@@ -10,7 +10,7 @@ type Position struct {
 	row, seat int
 }
 
-func (pos *Position) WithinArea() bool {
+func (pos *Position) withinArea() bool {
 	return !(pos.row < 0 || pos.row >= len(seatArea) || pos.seat < 0 || pos.seat >= len(seatArea[pos.row]))
 }
 
@@ -22,43 +22,43 @@ func (pos *Position) isOccupied() bool {
 	}
 }
 
-func (pos *Position) AdjacentNeighborsOccupied() int {
+func (pos *Position) adjacentNeighborsOccupied() int {
 	count := 0
 
 	for _, direction := range directions {
-		neighbor := pos.NeighborAt(direction)
-		if neighbor.WithinArea() && neighbor.isOccupied() {
+		neighbor := pos.neighborAt(direction)
+		if neighbor.withinArea() && neighbor.isOccupied() {
 			count = count + 1
 		}
 	}
 	return count
 }
 
-func (pos *Position) SeatWithinSightOccupied(direction Position) bool {
+func (pos *Position) seatWithinSightOccupied(direction Position) bool {
 	for i := 1; ; i++ {
 		posSight := Position{pos.row + direction.row*i, pos.seat + direction.seat*i}
-		if !posSight.WithinArea() {
+		if !posSight.withinArea() {
 			return false
-		} else if posSight.WithinArea() && seatArea[posSight.row][posSight.seat] == '#' {
+		} else if posSight.withinArea() && seatArea[posSight.row][posSight.seat] == '#' {
 			return true
-		} else if posSight.WithinArea() && seatArea[posSight.row][posSight.seat] == 'L' {
+		} else if posSight.withinArea() && seatArea[posSight.row][posSight.seat] == 'L' {
 			return false
 		}
 	}
 }
 
-func (pos *Position) NeighborsWithinSightOccupied() int {
+func (pos *Position) neighborsWithinSightOccupied() int {
 	count := 0
 
 	for _, direction := range directions {
-		if pos.SeatWithinSightOccupied(direction) {
+		if pos.seatWithinSightOccupied(direction) {
 			count = count + 1
 		}
 	}
 	return count
 }
 
-func (pos *Position) NeighborAt(direction Position) *Position {
+func (pos *Position) neighborAt(direction Position) *Position {
 	return &Position{pos.row + direction.row, pos.seat + direction.seat}
 }
 
@@ -78,26 +78,11 @@ var (
 	directions = []Position{
 		{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1},
 	}
-
 	seatArea = [][]rune{}
 )
 
-func main() {
-	file, err := os.Open("input.txt")
-	defer file.Close()
-	if err != nil {
-		panic(err)
-	}
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		row := []rune{}
-		for _, r := range scanner.Text() {
-			row = append(row, r)
-		}
-		seatArea = append(seatArea, row)
-	}
-
+func TaskOne(area [][]rune) {
+	seatArea = area
 	seatsChanged := true
 	for seatsChanged {
 		seatsChanged = false
@@ -107,7 +92,7 @@ func main() {
 			for seat, seatStatus := range seatArea[row] {
 				newSeatstatus := seatStatus
 				pos := Position{row, seat}
-				if occupiedSeats := pos.NeighborsWithinSightOccupied(); seatStatus == '#' && occupiedSeats >= 5 {
+				if occupiedSeats := pos.adjacentNeighborsOccupied(); seatStatus == '#' && occupiedSeats >= 4 {
 					newSeatstatus = 'L'
 					seatsChanged = true
 				} else if seatStatus == 'L' && occupiedSeats == 0 {
@@ -121,4 +106,50 @@ func main() {
 		seatArea = newSeatArea
 	}
 	fmt.Println("Task 1 -> Total amount of seats:", countTotalOccupiedSeats())
+}
+
+func TaskTwo(area [][]rune) {
+	seatArea = area
+	seatsChanged := true
+	for seatsChanged {
+		seatsChanged = false
+		newSeatArea := [][]rune{}
+		for row := range seatArea {
+			newRow := []rune{}
+			for seat, seatStatus := range seatArea[row] {
+				newSeatstatus := seatStatus
+				pos := Position{row, seat}
+				if occupiedSeats := pos.neighborsWithinSightOccupied(); seatStatus == '#' && occupiedSeats >= 5 {
+					newSeatstatus = 'L'
+					seatsChanged = true
+				} else if seatStatus == 'L' && occupiedSeats == 0 {
+					newSeatstatus = '#'
+					seatsChanged = true
+				}
+				newRow = append(newRow, newSeatstatus)
+			}
+			newSeatArea = append(newSeatArea, newRow)
+		}
+		seatArea = newSeatArea
+	}
+	fmt.Println("Task 2 -> Total amount of seats:", countTotalOccupiedSeats())
+}
+
+func main() {
+	file, err := os.Open("input.txt")
+	defer file.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	scannedArea := [][]rune{}
+	for scanner := bufio.NewScanner(file); scanner.Scan(); {
+		row := []rune{}
+		for _, r := range scanner.Text() {
+			row = append(row, r)
+		}
+		scannedArea = append(scannedArea, row)
+	}
+	TaskOne(scannedArea)
+	TaskTwo(scannedArea)
 }
