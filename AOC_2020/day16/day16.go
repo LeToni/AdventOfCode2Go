7e8652d3c32539533733e5357c976de8cd6dd81c
Toni Le <toni.le@outlook.de>
2021-01-07 22:16:02 +0100

    define struct for ticketfields and its lower bound and upper bound rules

--- AOC_2020/day16/day16.go
+++ AOC_2020/day16/day16.go
@@ -5,6 +5,32 @@ import (
 	"strings"
 )
 
+type Intervall struct {
+	lowerBound, upperBound int
+}
+
+func (inter *Intervall) withinBoundary(number int) bool {
+	if inter.lowerBound <= number && number <= inter.upperBound {
+		return true
+	} else {
+		return false
+	}
+}
+
+type TicketField struct {
+	rule1 Intervall
+	rule2 Intervall
+}
+
+func (tf *TicketField) isValid(number int) bool {
+
+	if tf.rule1.withinBoundary(number) || tf.rule2.withinBoundary(number) {
+		return true
+	} else {
+		return false
+	}
+}
+
 func main() {
 	file, err := ioutil.ReadFile("input.txt")
 	if err != nil {
